7e6a30ac5e93d21411a680246c50e75dd7b7ff67
Shanmuga <shanpriyamsp@gmail.com>
2026-08-06 05:31:39 +0530

    Add files via upload

---
+++ week1.day2/01_PrimitiveDataType.js
@@ -0,0 +1,12 @@
+
+let userName = "Shanmuga" 
+let organization = "TestLeaf"
+let contactNumber = 988701292 
+let knowsAutomation = true
+let usesPlaywright
+
+console.log("User Name: " + typeof(userName));
+console.log("Organization: " + typeof(organization));
+console.log("Contact Number: " + typeof(contactNumber));
+console.log("Knows Automation: " + typeof(knowsAutomation));
+console.log("Uses Playwright: " + typeof(usesPlaywright));
