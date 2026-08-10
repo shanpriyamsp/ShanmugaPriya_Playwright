1cfbe8a7a4496fb8d27c7848acf7a659d58356db
Shanmuga <shanpriyamsp@gmail.com>
2026-08-10 15:32:05 +0530

    Add files via upload

---
+++ week1.day4/01-Odd-Even.js
@@ -0,0 +1,15 @@
+//Learn to implement a JavaScript function determining whether a given 
+// //number is odd or even.
+function isOddOrEven(nNumber){
+    if(nNumber%2 == 0) 
+    {
+        console.log(nNumber + " is even")
+    }
+        else
+        {
+            console.log(nNumber + " is odd")
+        }
+    }
+
+    let nNumber = 21;
+    isOddOrEven(nNumber);
